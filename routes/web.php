552ac76e6c7d442f49f9adacf1bf5af3ac3d09ca
fallsnine <chairilpati11@gmail.com>
2023-08-17 22:14:34 +0700

    masih fix

--- routes/web.php
+++ routes/web.php
@@ -22,15 +22,11 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth','logLevel:admin')->group(function () {
+Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::middleware('auth','logLevel:admin,user')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    
-});
 
 require __DIR__.'/auth.php';
